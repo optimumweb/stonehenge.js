0fabd4d3e273a3e3e787823eedd649e130df34a2
Jonathan Roy <jroy66@gmail.com>
2023-03-26 13:43:29 -0400

    add autoscroll feature

--- dist/stonehenge.js
+++ dist/stonehenge.js
@@ -1,12 +1,20 @@
 (function($) {
     $.fn.stonehenge = function (options) {
         options = $.extend({
-            speed: 1.0
+            speed: 1.0,
+            autoscroll: false,
+            autoscrollSpeed: 25,
+            autoscrollPeriod: 250,
+            autoscrollEasing: 'linear',
         }, options);
 
         return this.each(function () {
             let $stonehenge = $(this),
                 speed = options.speed || $stonehenge.data('stonehenge-speed') || 1.0,
+                autoscroll = options.autoscroll || $stonehenge.data('stonehenge-autoscroll'),
+                autoscrollSpeed = options.autoscrollSpeed || $stonehenge.data('stonehenge-autoscroll-speed'),
+                autoscrollPeriod = options.autoscrollPeriod || $stonehenge.data('stonehenge-autoscroll-period'),
+                autoscrollEasing = options.autoscrollEasing || $stonehenge.data('stonehenge-autoscroll-easing'),
                 isGrabbed = false,
                 initialX,
                 scrollLeft;
@@ -33,6 +41,18 @@
                     let walk = (x - initialX) * speed;
                     this.scrollLeft = scrollLeft - walk;
                 });
+
+            if (autoscroll) {
+                let autoscrollDelta = autoscrollSpeed * autoscrollPeriod / 1000;
+
+                setInterval(function () {
+                    if (! isGrabbed) {
+                        $stonehenge.animate({
+                            scrollLeft: this.scrollLeft + autoscrollDelta
+                        }, autoscrollPeriod, autoscrollEasing);
+                    }
+                }, autoscrollPeriod);
+            }
         });
     };
 }(jQuery));
